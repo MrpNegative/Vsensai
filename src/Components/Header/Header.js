efae399f632d807e1fca1ff681a2107863b98c7f
mrnegativex <rashadul99924@gmail.com>
2022-04-19 17:07:34 +0600

    first commit

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Header.css'
+
+const Header = () => {
+    return (
+        <div className='header-container'>
+            <div className='header-info'>
+            <h1 className='h-title'>Unlock Your Acchivements</h1>
+            <p>Learn from the industry's best coaches at Gamer Sensei, the world's premier esports coaching platform. Start your journey and unlock your achievements.</p>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <div >
+      <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div class="container">
+    <Link class="navbar-brand fw-bolder" to="/">V-SENSAI</Link>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <Link class="nav-link active" aria-current="page" to="/">Home</Link>
+        </li>
+
+
+      </ul>
+     <div>
+       <button className="btn-light btn btn-sm fw-bold">SIGN IN / UP</button>
+     </div>
+    </div>
+  </div>
+</nav>
+    </div>
+  );
+};
+
+export default Navbar;
