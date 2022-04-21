8b707b8b7e717392248531778648da7d5a871639
mrnegativex <rashadul99924@gmail.com>
2022-04-22 00:12:32 +0600

    about me page added

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -2,8 +2,11 @@ import React from 'react';
 
 const About = () => {
     return (
-        <div>
-                <p>about</p>
+        <div className='container'>
+                <h1 className='text-center'>About Me</h1>
+                <hr />
+                <h3>Name: Md Rashadul Islam Fuhad</h3>
+                <p>Assalamualikum, Myself Fuhad am a  Inter 1st year student. For now, my goal is to finish this course in time with an average of 50+ marks so that I can join SCIC. My goal was to get an average of 60 but some problems came in the way and I had to late submit some assignments. After I submit this assignment I will try to come back to the flow of the course as before. And I plan to use the eid vacation to fill the gap I have.</p>
         </div>
     );
 };
